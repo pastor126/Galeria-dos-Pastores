6b167211327689e01c35ef724d0141c0713d0005
Eduardo C Pacheco <pastor126@gmail.com>
2024-11-27 19:48:46 +0000

    Permissão para app.

--- Galeria-dos-Pastores/src/main/java/com/pastor126/galeriap/security/WebSecurityConfig.java
+++ Galeria-dos-Pastores/src/main/java/com/pastor126/galeriap/security/WebSecurityConfig.java
@@ -64,6 +64,9 @@ public class WebSecurityConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
         config.addAllowedOrigin("https://pastor.up.railway.app");
+        config.addAllowedOrigin("android-app://com.eduardopacheco.galeriaflutter2");
+        config.addAllowedOrigin("http://localhost");
+        config.addAllowedOrigin("file://"); /////  CUIDAO !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
         config.addAllowedHeader("*"); //Permite todos os cabeçalhos.
         config.addAllowedMethod("*"); //Permite todos os métodos HTTP.
         source.registerCorsConfiguration("/**", config); //Registra a configuração de CORS para todas as rotas (/**).
